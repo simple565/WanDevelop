f9971bdf0e33a167a1be49dc5853f8cfdb4277c3
一车四木 <467914885@qq.com>
2025-04-13 20:03:04 +0800

    增加主题Preview注解

---
+++ app/src/main/java/com/maureen/wandevelop/common/tooling/Previews.kt
@@ -0,0 +1,9 @@
+package com.maureen.wandevelop.common.tooling
+
+import android.content.res.Configuration
+import androidx.compose.ui.tooling.preview.Preview
+
+
+@Preview(uiMode = Configuration.UI_MODE_NIGHT_NO, name = "Light theme")
+@Preview(uiMode = Configuration.UI_MODE_NIGHT_YES, name = "Dark theme")
+annotation class UiModePreviews
