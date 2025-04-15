571c5a3fc3807a0ddc1c3e4d7afcf7228837cae0
一车四木 <467914885@qq.com>
2025-04-15 22:03:02 +0800

    新增关于页面

---
+++ app/src/main/java/com/maureen/wandevelop/feature/setting/AboutPage.kt
@@ -0,0 +1,100 @@
+package com.maureen.wandevelop.feature.setting
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.unit.dp
+import com.maureen.wandevelop.R
+import com.maureen.wandevelop.common.composable.WanDevTpoAppBar
+import com.maureen.wandevelop.ext.curVersionName
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+internal fun AboutPage(
+    onBackClick: () -> Unit,
+    modifier: Modifier = Modifier
+) {
+    Scaffold(
+        modifier = modifier.fillMaxSize(),
+        topBar = {
+            WanDevTpoAppBar(
+                title = stringResource(R.string.nav_about_us),
+                onNavigationIconClick = onBackClick
+            )
+        },
+        content = { paddingValues ->
+            Column(
+                modifier = Modifier
+                    .fillMaxSize()
+                    .padding(paddingValues),
+                verticalArrangement = Arrangement.SpaceBetween,
+                horizontalAlignment = Alignment.CenterHorizontally
+            ) {
+                Column(
+                    modifier = Modifier.fillMaxWidth(),
+                    verticalArrangement = Arrangement.Center,
+                    horizontalAlignment = Alignment.CenterHorizontally
+                ) {
+                    Image(
+                        painter = painterResource(R.mipmap.ic_launcher),
+                        contentDescription = null,
+                        modifier = Modifier.size(100.dp),
+                        contentScale = ContentScale.Fit
+                    )
+
+                    Text(
+                        text = stringResource(R.string.app_name),
+                        style = MaterialTheme.typography.titleLarge
+                    )
+
+                    Text(
+                        text = LocalContext.current.applicationContext.curVersionName,
+                        style = MaterialTheme.typography.bodySmall,
+                        color = MaterialTheme.colorScheme.onSurfaceVariant,
+                        modifier = Modifier.padding(top = 6.dp)
+                    )
+                }
+
+                Column(
+                    modifier = Modifier.fillMaxWidth(),
+                    verticalArrangement = Arrangement.Center,
+                    horizontalAlignment = Alignment.CenterHorizontally
+                ) {
+                    Text(
+                        text = LocalContext.current.applicationContext.curVersionName,
+                        style = MaterialTheme.typography.bodySmall,
+                        color = MaterialTheme.colorScheme.onSurfaceVariant,
+                        modifier = Modifier.padding(top = 6.dp)
+                    )
+                    Text(
+                        text = LocalContext.current.applicationContext.curVersionName,
+                        style = MaterialTheme.typography.bodySmall,
+                        color = MaterialTheme.colorScheme.onSurfaceVariant,
+                        modifier = Modifier.padding(top = 6.dp)
+                    )
+                    Text(
+                        text = LocalContext.current.applicationContext.curVersionName,
+                        style = MaterialTheme.typography.bodySmall,
+                        color = MaterialTheme.colorScheme.onSurfaceVariant,
+                        modifier = Modifier.padding(vertical = 6.dp)
+                    )
+                }
+            }
+        }
+    )
+}
---
+++ app/src/main/java/com/maureen/wandevelop/feature/setting/SettingRoute.kt
@@ -0,0 +1,27 @@
+package com.maureen.wandevelop.feature.setting
+
+import androidx.navigation.NavGraphBuilder
+import androidx.navigation.compose.composable
+import kotlinx.serialization.Serializable
+
+@Serializable
+object AboutRoute
+
+@Serializable
+object SettingRoute
+
+fun NavGraphBuilder.settingScreens(
+    onBackClick: () -> Unit
+) {
+    composable<AboutRoute> {
+        AboutPage(
+            onBackClick = onBackClick
+        )
+    }
+    composable<SettingRoute> {
+        SettingsPage(
+            onBackClick = onBackClick
+        )
+    }
+}
+
