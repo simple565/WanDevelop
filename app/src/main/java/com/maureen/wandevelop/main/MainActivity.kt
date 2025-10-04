8cc70249de284269e0acf1515106466fb2d4db6d
一车四木 <467914885@qq.com>
2025-10-04 11:19:23 +0800

    替换废弃系统api

--- app/src/main/java/com/maureen/wandevelop/main/MainActivity.kt
+++ app/src/main/java/com/maureen/wandevelop/main/MainActivity.kt
@@ -1,11 +1,10 @@
 package com.maureen.wandevelop.main
 
-import android.os.Build
 import android.os.Bundle
-import android.view.WindowManager
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.core.view.ViewCompat
 import com.maureen.wandevelop.main.ui.WanDevelopApp
 import com.maureen.wandevelop.main.ui.rememberWanDevAppState
 import com.maureen.wandevelop.ui.theme.WanDevelopTheme
@@ -15,11 +14,9 @@ class MainActivity : ComponentActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        // 沉浸式导航栏适配小米手机
-        if (Build.MANUFACTURER.equals("xiaomi", true)) {
-            window.addFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_NAVIGATION)
-        }
         enableEdgeToEdge()
+        // 沉浸式导航栏适配小米手机
+        ViewCompat.setOnApplyWindowInsetsListener(window.decorView) { _, insets -> insets }
         setContent {
             val appState = rememberWanDevAppState()
             WanDevelopTheme {
